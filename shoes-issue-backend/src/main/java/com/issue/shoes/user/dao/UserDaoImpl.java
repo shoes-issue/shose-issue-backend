b5133d99d6fb895dc9fa920597af1d69f6f7b100
Newbi7 <anelitgel12@gmail.com>
2023-03-10 17:19:04 +0900

    user spring 작성

---
+++ shoes-issue-backend/src/main/java/com/issue/shoes/user/dao/UserDaoImpl.java
@@ -0,0 +1,59 @@
+package com.issue.shoes.user.dao;
+
+import org.apache.ibatis.session.SqlSession;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Repository;
+
+import com.issue.shoes.user.vo.User;
+
+@Repository
+public class UserDaoImpl implements UserDao {
+	
+	@Autowired
+	private SqlSession session;
+	
+	@Override
+	public User selectUserById(String userId) {
+	    User result = null;
+	    
+	    result = session.selectOne("User.insertUser", userId);
+	    
+	    return result;
+	}
+	
+	@Override
+	public void insertUser(User user) throws Exception {
+	    int result = 0;
+	    
+	    result = session.insert("User.insertUser", user);
+	   
+	    if (result != 1) {
+	    	throw new Exception();
+	    }
+	}
+	
+	@Override
+	public void updateUser(User user) throws Exception {
+	    int result = 0;
+	    
+	    result = session.update("User.updateUser", user);
+	   
+	    if (result != 1) {
+	    	throw new Exception();
+	    }
+	}
+
+	@Override
+	public void deleteUser(String userId) throws Exception {
+	    int result = 0;
+	    
+	    result = session.delete("User.deleteUser", userId);
+	   
+	    if (result != 1) {
+	    	throw new Exception();
+	    }
+	}
+
+}
+
+	
---
+++ shoes-issue-backend/src/main/java/com/issue/shoes/user/service/UserCRUDService.java
@@ -0,0 +1,51 @@
+package com.issue.shoes.user.service;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.PlatformTransactionManager;
+import org.springframework.transaction.annotation.Transactional;
+import org.springframework.web.bind.annotation.CrossOrigin;
+
+import com.issue.shoes.user.dao.UserDao;
+import com.issue.shoes.user.vo.User;
+
+@Service
+public class UserCRUDService implements UserService {
+
+	@Autowired
+	private UserDao userDao;
+	private PlatformTransactionManager transactionManager;
+	
+	public UserCRUDService(PlatformTransactionManager transactionManager) {
+	    this.transactionManager = transactionManager;
+	}
+
+	@Transactional
+	@Override
+	public String searchUser(String userId) {
+		// userId를 사용하여 유저 검색 기능 구현
+		return userId;
+	}
+
+	@Transactional
+	@Override
+	public User createUser(User user) throws Exception {
+	    userDao.insertUser(user);
+	    return user;
+	}
+
+	@Transactional
+	@Override
+	public User updateUser(User user) throws Exception {
+	    userDao.updateUser(user);
+	    return user;
+	}
+
+	@Transactional
+	@Override
+	public String deleteUser(String userId) throws Exception {
+	    userDao.deleteUser(userId);
+	    return userId;
+	}
+
+}
