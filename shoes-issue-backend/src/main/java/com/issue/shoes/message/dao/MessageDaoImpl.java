56f216613f27f030b2c8870bef48a9e253b109af
eunseo0423 <kyee0423@gmail.com>
2023-03-10 09:44:34 +0900

    20230309

---
+++ shoes-issue-backend/src/main/java/com/issue/shoes/message/dao/MessageDaoImpl.java
@@ -0,0 +1,15 @@
+package com.issue.shoes.message.dao;
+
+import org.apache.ibatis.session.SqlSession;
+
+public class MessageDaoImpl implements MessageDao {
+
+	private SqlSession session;
+
+	public String createMessage() {
+		
+		return null;
+	}
+	
+	
+}
