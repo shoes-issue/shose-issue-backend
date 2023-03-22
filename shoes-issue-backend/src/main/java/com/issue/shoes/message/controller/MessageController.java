3d437599accbec5acc25540eb07b4da98f4eae0a
eunseo0423 <kyee0423@gmail.com>
2023-03-22 18:32:17 +0900

    닉네임 보이기 완성

--- shoes-issue-backend/src/main/java/com/issue/shoes/message/controller/MessageController.java
+++ shoes-issue-backend/src/main/java/com/issue/shoes/message/controller/MessageController.java
@@ -7,20 +7,29 @@ import org.springframework.http.ResponseEntity;
 import com.issue.shoes.message.vo.Message;
 
 public interface MessageController {
-	
-	//쪽지 전송
+
+	// 쪽지 전송
 	ResponseEntity<String> sendMessage(Message message);
-	//쪽지 삭제
+
+	// 쪽지 삭제
 	ResponseEntity<String> deleteMessage(Message messageId);
-	//받은 쪽지함
+
+	// 받은 쪽지함
 	List<Message> allReceivedMessage(String messageReceiver);
-	//보낸 쪽지함
+
+	// 보낸 쪽지함
 	List<Message> allSendMessage(String messageReceiver);
-	//쪽지 열람
-	Message openMessageDetail(String messageId);
-	//쪽지 열람여부 확인
+
+	// 받은 쪽지 열람
+	Message openReceiveMessageDetail(String messageId);
+
+	// 보낸 쪽지 열람
+	Message openSendMessageDetail(String messageId);
+
+	// 쪽지 열람여부 확인
 	String openMessageStatus();
-	//카톡으로 알림 보내기
+
+	// 카톡으로 알림 보내기
 	String alertKakao();
 
 }
--- shoes-issue-backend/src/main/java/com/issue/shoes/message/controller/MessageControllerImpl.java
+++ shoes-issue-backend/src/main/java/com/issue/shoes/message/controller/MessageControllerImpl.java
@@ -26,71 +26,69 @@ import com.issue.shoes.message.vo.Message;
 @RestController
 @RequestMapping(value = "message")
 public class MessageControllerImpl implements MessageController {
-	
+
 	Logger log = LogManager.getLogger("case3");
-	
+
 	@Autowired
 	private Gson gson;
-	
+
 	@Autowired
 	private MessageServiceImpl service;
-	
-	
-	//쪽지 보내기(생성)
+
+	// 쪽지 보내기(생성)
 	@Override
 	@PostMapping(produces = "application/json; charset=UTF-8")
 	public ResponseEntity<String> sendMessage(@RequestBody Message message) {
 		log.debug("sendMessage 실행={}", message);
-		
+
 		UUID uuid = UUID.randomUUID();
 		message.setMessageId(uuid.toString());
-		
+
 		int result = service.sendMessage(message);
-		// status 
-		if(result == 1 ) {
+		// status
+		if (result == 1) {
 			log.debug("쪽지 생성 성공");
 		} else {
 			log.debug("이상이 있어요");
 		}
 		log.debug("결과={}", result);
-		
+
 		String jsonResult = gson.toJson("쪽지 생성 완료");
-		
+
 		return new ResponseEntity<String>(jsonResult, HttpStatus.OK);
 	}
 
-	
 	// 쪽지 삭제
 	@Override
 	@DeleteMapping(value = "{messageId}")
 	public ResponseEntity<String> deleteMessage(@PathVariable Message message) {
 		log.debug("deleteMessage 실행={}", message);
-		
+
 		String jsonResult = gson.toJson("쪽지 삭제 완료");
-		
+
 		return new ResponseEntity<String>(jsonResult, HttpStatus.OK);
 	}
 
-	
 	// 받은 쪽지함`
 	// messageReciever(받은 사람) == userId
-	// URL  : message/inbox
-	// GET방식에서  param에 보내주는 방법하고, URL에 붙여주는 방법이 있늑ㄴ것 같아요
+	// URL : message/inbox
+	// GET방식에서 param에 보내주는 방법하고, URL에 붙여주는 방법이 있늑ㄴ것 같아요
 	// 이 둘중에 하나를 사용하시면 될 것 같아요
 	@Override
-	@GetMapping(value = "inbox/{messageReceiver}", produces ="application/json; charset= UTF-8") // produces 응답하는 데이터 형식? 일꺼에요
+	@GetMapping(value = "inbox/{messageReceiver}", produces = "application/json; charset= UTF-8") // produces 응답하는 데이터
+																									// 형식? 일꺼에요
 	@ResponseBody
 	public List<Message> allReceivedMessage(@PathVariable("messageReceiver") String messageReceiver) {
 		log.debug("messageReceiver 조회={}", messageReceiver);
 		// message/inbox?messageReceiver=asdfjkl
 		List<Message> result = service.findAllReceivedMessage(messageReceiver);
-		
+
 		return result;
 	}
-	
+
 	// 보낸 쪽지함
 	@Override
-	@GetMapping(value="sent/{messageSender}", produces ="application/json; charset= UTF-8")
+	@GetMapping(value = "sent/{messageSender}", produces = "application/json; charset= UTF-8")
 	public List<Message> allSendMessage(@PathVariable("messageSender") String messageSender) {
 		log.debug("messageSender 조회={}", messageSender);
 
@@ -98,27 +96,52 @@ public class MessageControllerImpl implements MessageController {
 
 		return result;
 	}
-	
-	// 쪽지 상세 정보 조회
+
+	// 받은 쪽지 상세 정보 조회
 	// 메시지 한개의 세부정보를 확인하는거죠
 	// 회원 아이디랑, messageID가 있으면 확인할 수 있어요
 	@Override
 	@GetMapping(value = "{messageId}", produces = "application/json; charset= UTF-8")
-	public Message openMessageDetail(@PathVariable("messageId") String messageId) {
+	public Message openReceiveMessageDetail(@PathVariable("messageId") String messageId) {
 		// http://localhost:80/message/{messageId} 의 주소로 GET요청을 보내면 여기로 도착할 꺼에요
-		System.out.println(messageId+ "입니다");
+		System.out.println(messageId + "입니다");
 		// 1. 일단 여기로 요청이 들어와야 해요
 		log.debug("messageId 조회={}", messageId);
-		
-		Message result = service.findOneMessage(messageId);
+
+		Message result = service.findOneReceiveMessage(messageId);
 		// 유저의 아이디하고 messageid가 같으면 이라는 조건을 DB에서 사용해서 메시지의 내용으 가져와요
 		// Message result = service.searchMessageDetaio(message);
-		// result == null 하면은 이 값을 처리해주고 
-		log.debug("예시 결과={}",result);
-				
+		// result == null 하면은 이 값을 처리해주고
+		log.debug("예시 결과={}", result);
+
 		/// 하나의 결과가 나오던가 결과가 null일 꺼에요
 		// service에서 나온 결과를 어떻게 처리 해야해요
-		
+
+		// 그 결과를 return 해주면 됩니다.
+		// vo를 내보내주면 spring이 알아서 json으로 바꿔서 받을 수 있게 보내줘요
+		return result;
+	}
+
+	// 보낸 쪽지 상세 정보 조회
+	// 메시지 한개의 세부정보를 확인하는거죠
+	// 회원 아이디랑, messageID가 있으면 확인할 수 있어요
+	@Override
+	@GetMapping(value = "{messageId}/send", produces = "application/json; charset= UTF-8")
+	public Message openSendMessageDetail(@PathVariable("messageId") String messageId) {
+		// http://localhost:80/message/{messageId} 의 주소로 GET요청을 보내면 여기로 도착할 꺼에요
+		System.out.println(messageId + "입니다");
+		// 1. 일단 여기로 요청이 들어와야 해요
+		log.debug("messageId 조회={}", messageId);
+
+		Message result = service.findOneSendMessage(messageId);
+		// 유저의 아이디하고 messageid가 같으면 이라는 조건을 DB에서 사용해서 메시지의 내용으 가져와요
+		// Message result = service.searchMessageDetaio(message);
+		// result == null 하면은 이 값을 처리해주고
+		log.debug("예시 결과={}", result);
+
+		/// 하나의 결과가 나오던가 결과가 null일 꺼에요
+		// service에서 나온 결과를 어떻게 처리 해야해요
+
 		// 그 결과를 return 해주면 됩니다.
 		// vo를 내보내주면 spring이 알아서 json으로 바꿔서 받을 수 있게 보내줘요
 		return result;
